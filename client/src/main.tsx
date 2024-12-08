09d580814f261099071acd477c0f8e68886bbb74
Nadav Sagiv <nadavmichaelsagiv@gmail.com>
2024-12-08 18:01:05 +0200

    Fix font imports for Vercel deployment

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import '@fontsource/rubik';
 import '@fontsource/rubik/400.css';
 import '@fontsource/rubik/500.css';
 import '@fontsource/rubik/700.css';
